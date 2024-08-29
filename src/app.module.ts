358ebd350353e9617fc139710a10ab8039d1bd89
zhouyiping <zhouyiping_love@126.com>
2024-08-29 18:30:50 +0800

    添加配置说明

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { join } from 'path'
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       path: 'bff/graphql', // 指定 GraphQL 请求的路径 =>  默认情况下，GraphQL 服务器通常在 /graphql 路径上运行。 http://127.0.0.1:9000/graphql => 现需要改成 http://127.0.0.1:9000/bff/graphql
-      driver: ApolloDriver,
+      driver: ApolloDriver, // 指定 GraphQL 服务器引擎 => ApolloDriver 是 Apollo Server 的实现，用于处理 GraphQL 请求和执行
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // 自动生成 schema 文件
       debug: true, // 启用调试模式，输出更详细的错误信息。
       playground: true // 启用 GraphQL Playground
