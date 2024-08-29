8a8d77fe76a6bbf7e40f495b248be82d0436e8f5
zhouyiping <zhouyiping_love@126.com>
2024-08-29 18:27:05 +0800

    指定 GraphQL 请求的路径

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { join } from 'path'
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
+      path: 'bff/graphql', // 指定 GraphQL 请求的路径 =>  默认情况下，GraphQL 服务器通常在 /graphql 路径上运行。 http://127.0.0.1:9000/graphql => 现需要改成 http://127.0.0.1:9000/bff/graphql
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       debug: true,
